ece6d23b39b0233343146f08d64e928ed7359b87
thirumal97 <thirumalkonatham1@gmail.com>
2020-06-19 01:26:17 +0000

    file is changed again

--- example.py
+++ example.py
@@ -1,4 +1,4 @@
 def git_operation():
-    print("I am adding example.py file to the remote repository.")
+    print("I am adding example.py file to the remote repository and the detials.")
 git_operation()
 
